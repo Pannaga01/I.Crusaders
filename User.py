8ebd73b6e66075f42302b2fd6f50e1a5855afae8
Pannaga01 <53700154+Pannaga01@users.noreply.github.com>
2022-03-28 13:15:37 +0530

    Update User.py

--- User.py
+++ User.py
@@ -7,7 +7,7 @@ class User:
         self.text = text
         self.key = key
         self.path = path
-88
+
     def encrypt_or_decrypt(self):
         
         if self.algo == "shift_cipher":
@@ -17,6 +17,24 @@ class User:
                 obj.encrypt()
             else:
                 obj = shift_cipher(self.key,None,self.text)
+                obj.decrypt()
+                
+        if self.algo == "hillcypher":
+            
+            if option == "encrypt":
+                obj = hillcypher(self.key,self.text,None)
+                obj.encrypt()
+            else:
+                obj = hillcypher(self.key,None,self.text)
+                obj.decrypt()
+
+        if self.algo == "caesarcipher":
+            
+            if option == "encrypt":
+                obj = caesarcipher(self.key,self.text,None)
+                obj.encrypt()
+            else:
+                obj = caesarcipher(self.key,None,self.text)
                 obj.decrypt()
 
         obj.dsiplay()
