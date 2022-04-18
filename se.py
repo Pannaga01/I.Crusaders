332ca07239a302c8b1fb5c036bbb799e023f612e
Vinayak R Pillai <52862736+Vinayak730@users.noreply.github.com>
2022-04-18 10:37:35 +0530

    update se.py
    updated view button

--- se.py
+++ se.py
@@ -8,8 +8,8 @@ master = Tk()
 screen_width = master.winfo_screenwidth()
 screen_height = master.winfo_screenheight()
 
-window_width = 500
-window_height = 500
+window_width = 360
+window_height = 360
 
 xCoordinate = int(screen_width/2 - window_width/2)
 yCoordinate = int(screen_height/2 - window_height/2)
@@ -23,17 +23,19 @@ for i in range(1, 5):
     master.rowconfigure(i, weight = 1)
 
 master.columnconfigure(0, weight = 1)
-master.columnconfigure(1, weight = 1)
+master.columnconfigure(1, weight = 1)  
 master.columnconfigure(2, weight = 1)
 
 #button_width = 25
-e = Button(master, text = 'ENCRYPT', command = lambda: openWindow(master, screen_width, screen_height, 'Encrypt'))
+e  = Button(master, text = 'ENCRYPT', command = lambda: openWindow(master, screen_width, screen_height, 'Encrypt'),)
+d  = Button(master, text = 'DECRYPT', command = lambda: openWindow(master, screen_width, screen_height, 'Decrypt'))
+v  = Button(master, text = 'VIEW', command = lambda: openWindow(master, screen_width, screen_height, 'view'))
+ex = Button(master, text = "Exit",command = master.destroy)
 
-d = Button(master, text = 'DECRYPT')
-v = Button(master, text = 'VIEW')
-e.grid(column = 1, row = 1, sticky = 'nesw', pady = 10)
-d.grid(column = 1, row = 2, sticky = 'nesw', pady = 10)
-v.grid(column = 1, row = 3, sticky = 'nesw', pady = 10)
+e.grid( column = 1, row = 1, sticky = 'nesw', pady = 10)
+d.grid( column = 1, row = 2, sticky = 'nesw', pady = 10)
+v.grid( column = 1, row = 3, sticky = 'nesw', pady = 10)
+ex.grid(column = 1, row = 4, sticky = 'nesw', pady = 10)
 
 master.mainloop()
 
