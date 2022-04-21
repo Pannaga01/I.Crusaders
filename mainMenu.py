39eb5720afc90bc517c7da20bc9451e98dcf81fc
klassikboi <61735876+klassikboi@users.noreply.github.com>
2022-04-21 17:32:49 +0530

    Update mainMenu.py

--- mainMenu.py
+++ mainMenu.py
@@ -9,24 +9,6 @@ window_width = 550
 window_height = 400
 file_contents = ''  # contains the contents of the file we choose
 
-''' 
-this function is bound to the Encrypt/Decrypt button. We call the encryption/decryption algo in this
-function and insert the result (plain text/cipher text) in the text box 
-'''
-
-def selectAlgo(opt, key, textBox, e_or_d):
-    global file_contents
-    if not e_or_d:
-        cc = CaesarCipher(int(key))
-        cc.plain_text = file_contents
-        cc.encryption()
-        textBox.insert(END, cc.cipher_text)
-    else:
-        cc = CaesarCipher(-1*int(key))
-        cc.plain_text = file_contents
-        cc.encryption()
-        textBox.insert(END, cc.cipher_text)
-
 # This function is bound to the Browse Files button. The uploaded file's contents are stored in file_contents
 def openFile():
     file = filedialog.askopenfilename(
@@ -67,12 +49,13 @@ def openWindow(master, screen_width, screen_height, title):
     newWindow = Toplevel(master)
     newWindow.title(title)
     global window_height, window_width
-    if title == 'Encrypt':
-        e_d_Window(newWindow, 0)
-    elif title == "view":
-        view_window(newWindow)
-    else: 
-        e_d_Window(newWindow, 1)
+    
+    xCoordinate = int(screen_width/2 - window_width/2)
+    yCoordinate = int(screen_height/2 - window_height/2)
+    newWindow.geometry('{}x{}+{}+{}'.format(window_width, window_height, xCoordinate, yCoordinate)) 
+    if title == 'Encrypt': e_d_Window(newWindow, 0)
+    elif title == 'Decrypt': e_d_Window(newWindow, 1)
+    else: view_window(newWindow)
 
 # this function basically deals with all the contents inside the encrypt/decrypt window.
 def view_window(window):
@@ -83,7 +66,7 @@ def view_window(window):
 
 def e_d_Window(window, e_or_d):
     global window_height, window_width
-    for i in range(9):
+    for i in range(11):
         window.rowconfigure(i, weight = 1)
 
     window.columnconfigure(0, weight = 1)
@@ -93,55 +76,107 @@ def e_d_Window(window, e_or_d):
 
     # option contains the value of the radiobutton
     option = IntVar()
-    option1 = Radiobutton(window, text = 'Caesar Cipher', variable = option, value = 0)
+    option1 = Radiobutton(window, text = 'Shift Cipher', variable = option, value = 0)
 
     '''
     this frame and text box is the one under the Caesar cipher radiobutton for taking Caesar cipher's key.
     Note: It's common practice to put text boxes inside their own frames to be able to size them properly.
     I've done the same for all text boxes in this program.
     '''
-    ccFrame = Frame(window, borderwidth = 1, relief = 'sunken', height = 50, width = 100)
-    ccKey = Text(ccFrame)
+    scFrame = Frame(window, borderwidth = 1, relief = 'sunken', height = 50, width = 100)
+    scKey = Text(scFrame)
 
     option1.grid(column = 1, row = 2, sticky = W)
-    ccFrame.grid(column = 1, row = 3, sticky = NW)
-    ccFrame.grid_propagate(False)
-    ccKey.grid()
+    scFrame.grid(column = 1, row = 3, sticky = NW)
+    scFrame.grid_propagate(False)
+    scKey.grid()
 
-    option2 = Radiobutton(window, text = 'Hill Cipher', variable = option, value = 1)
+    option2 = Radiobutton(window, text = 'Rail Fence Cipher', variable = option, value = 1)
     option2.grid(column = 1, row = 4, sticky = W)
 
-    # frame and text box for input of hill cipher's key 
-    hcFrame = Frame(window, borderwidth = 1, relief = 'sunken', height = 50, width = 100)
-    hcKey = Text(hcFrame)
+    # frame and text box for input of rail fence cipher's key 
+    rfcFrame = Frame(window, borderwidth = 1, relief = 'sunken', height = 50, width = 100)
+    rfcKey = Text(rfcFrame)
 
-    hcFrame.grid(column = 1, row = 5, sticky = NW)
-    hcFrame.grid_propagate(False)
-    hcKey.grid()
+    rfcFrame.grid(column = 1, row = 5, sticky = NW)
+    rfcFrame.grid_propagate(False)
+    rfcKey.grid()
+
+    option3 = Radiobutton(window, text = 'Vignere Cipher', variable = option, value = 2)
+    option3.grid(column = 1, row = 6, sticky = W)
+
+    # frame and text box for input of vignere cipher's key 
+    vcFrame = Frame(window, borderwidth = 1, relief = 'sunken', height = 50, width = 100)
+    vcKey = Text(vcFrame)
+
+    vcFrame.grid(column = 1, row = 7, sticky = NW)
+    vcFrame.grid_propagate(False)
+    vcKey.grid()
     
     '''
     frame and text box for the text box on the right side of the window where we display output of
     encryption/decryption algorithm
     '''
     container = Frame(window, borderwidth = 1, relief = 'sunken', height = window_height/1.1, width = window_width/1.5)
-    container.grid(column = 2, row = 1, rowspan = 6, pady = 20)
+    container.grid(column = 2, row = 1, rowspan = 9, pady = 20)
     container.grid_propagate(False)
     rawText = Text(container)
     rawText.grid()
 
     # Encrypt/Decrypt button at the bottom
     if not e_or_d:
-        e_or_dButton = Button(window, text = 'Encrypt', command = lambda: selectAlgo(option.get(), ccKey.get('1.0', END), rawText, 0))
+        e_or_dButton = Button(window, text = 'Encrypt', command = lambda: selectAlgo(option.get(), [scKey.get('1.0',END), rfcKey.get('1.0',END), vcKey.get('1.0',END)], rawText, 0))
     else:
-        e_or_dButton = Button(window, text = 'Decrypt', command = lambda: selectAlgo(option.get(), ccKey.get('1.0', END), rawText, 1))
+        e_or_dButton = Button(window, text = 'Decrypt', command = lambda: selectAlgo(option.get(), [scKey.get('1.0',END), rfcKey.get('1.0',END), vcKey.get('1.0',END)], rawText, 1))
 
-    e_or_dButton.grid(column = 1, row = 6, sticky = W)
+    e_or_dButton.grid(column = 1, row = 8, sticky = W)
 
-    save = Button(window, text = 'save',command=lambda:save_file(rawText.get('1.0',END)))
-    save.grid(column = 1,row = 7,sticky = W)
+    save = Button(window, text = 'Save',command=lambda:save_file(rawText.get('1.0',END)))
+    save.grid(column = 1,row = 9,sticky = W)
 
     # Browse files button
     chooseFile = Button(window, text = 'Browse Files', command = lambda: openFile())
     chooseFile.grid(column = 1, row = 1, sticky = W)
 
     window.mainloop()
+
+''' 
+this function is bound to the Encrypt/Decrypt button. We call the encryption/decryption algo in this
+function and insert the result (plain text/cipher text) in the text box 
+'''
+
+def selectAlgo(opt, keys, textBox, e_or_d):
+    global file_contents, scKey
+    if not opt:
+        if not e_or_d:
+            sc = ShiftCipher(int(keys[0]))
+            sc.plain_text = file_contents
+            sc.encryption()
+            textBox.insert(END, sc.cipher_text)
+        else:
+            sc = ShiftCipher(-1*int(keys[0]))
+            sc.plain_text = file_contents
+            sc.encryption()
+            textBox.insert(END, sc.cipher_text)
+    elif opt == 1:
+        if not e_or_d:
+            rfc = RailFence(int(keys[1]))
+            rfc.plain_text = file_contents
+            rfc.encryption()
+            textBox.insert(END, rfc.cipher_text)
+        else:
+            rfc = RailFence(int(keys[1]))
+            rfc.cipher_text = file_contents
+            rfc.decryption()
+            textBox.insert(END, rfc.plain_text)
+    elif opt == 2:
+        if not e_or_d:
+            vc = VigenereCipher(keys[2])
+            vc.plain_text = file_contents
+            vc.encryption()
+            textBox.insert(END, vc.cipher_text)
+        else:
+            vc = VignereCipher(keys[2])
+            vc.cipher_text = file_contents
+            vc.encryption()
+            textBox.insert(END, vc.cipher_text)
